053f3e6bc74bd81a5e7a5b73ca18ae33017e3de4
stormy <stormy@mailfence.com>
2024-05-30 16:10:40 +0200

    buffer: add buf_getu24 function for jag archive access

--- src/buffer.c
+++ src/buffer.c
@@ -43,6 +43,21 @@ buf_getu16(void *b, size_t offset, size_t buflen, uint16_t *out)
 }
 
 int
+buf_getu24(void *b, size_t offset, size_t buflen, uint32_t *out)
+{
+	uint8_t *buffer = b;
+
+	if (offset > (SIZE_MAX - 3) ||
+	    ((ptrdiff_t)buflen - (ptrdiff_t)offset) < 3) {
+		return -1;
+	}
+	*out = ((buffer[offset + 0] & 0xff) << 16) |
+	       ((buffer[offset + 1] & 0xff) << 8) |
+	        (buffer[offset + 2] & 0xff);
+	return 0;
+}
+
+int
 buf_getu32(void *b, size_t offset, size_t buflen, uint32_t *out)
 {
 	uint8_t *buffer = b;
--- src/buffer.h
+++ src/buffer.h
@@ -5,6 +5,7 @@
 
 int buf_getu8(void *, size_t, size_t, uint8_t *);
 int buf_getu16(void *, size_t, size_t, uint16_t *);
+int buf_getu24(void *, size_t, size_t, uint32_t *);
 int buf_getu32(void *, size_t, size_t, uint32_t *);
 int buf_gets64(void *, size_t, size_t, int64_t *);
 
