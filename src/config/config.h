b2843ed9147bae1c3a8708e20a95dcb72c6ec708
Stormy <stormy@mailfence.com>
2024-05-31 12:46:57 +0200

    fill in equip_type enum

--- src/config/config.h
+++ src/config/config.h
@@ -7,6 +7,23 @@
 
 /* entities defined in config.jag */
 
+enum equip_type {
+	EQUIP_TYPE_SHIELD	= 0x0008,
+	EQUIP_TYPE_WEAPON	= 0x0010,
+	EQUIP_TYPE_WEAPON_2H	= 0x2018,
+	EQUIP_TYPE_HEAD		= 0x0020,
+	EQUIP_TYPE_HEAD_FULL	= 0x0021,
+	EQUIP_TYPE_TORSO	= 0x0040,
+	EQUIP_TYPE_TORSO_FULL	= 0x0142,
+	EQUIP_TYPE_GLOVES	= 0x0100,
+	EQUIP_TYPE_LEG		= 0x0080,
+	EQUIP_TYPE_LEG_FULL	= 0x0280,
+	EQUIP_TYPE_LEG_FULL2	= 0x0284,
+	EQUIP_TYPE_SHOES	= 0x0200,
+	EQUIP_TYPE_NECK		= 0x0400,
+	EQUIP_TYPE_BACK		= 0x0800
+};
+
 struct item_config {
 	uint16_t id;
 	uint16_t name_count;
@@ -23,7 +40,7 @@ struct item_config {
 	uint8_t bonus_prayer;
 	uint8_t bonus_hiding; /* not used */
 	char *projectile;
-	uint32_t equip_type;
+	uint16_t equip_type;
 	char *animation;
 	uint32_t colour;
 	uint16_t respawn_rate;
