83d5a3b27c5aa8d4b13f2cf1675a1afa3500b545
Stormy <stormy@mailfence.com>
2024-06-03 08:45:27 +0200

    fetch-jag-files: print checksum on match too

--- data/fetch-jag-files.sh
+++ data/fetch-jag-files.sh
@@ -47,6 +47,7 @@ if ! [ -e config46.jag ]; then
 fi
 printf "verifying config46.jag\n"
 hash=$(${CKSUM} < config46.jag | cut -d' ' -f1)
+printf "got checksum %s\n" "${hash}"
 target='304ff7147a21119c4e47f913f5d27d65de8222d69d2f3317dff569d8a4061e94'
 if [ "${hash}" != "${target}" ]; then
 	printf "WARNING: config46.jag's checksum is wrong\n" >/dev/stderr
@@ -61,6 +62,7 @@ if ! [ -e maps27.jag ]; then
 fi
 printf "verifying maps27.jag\n"
 hash=$(${CKSUM} < maps27.jag | cut -d' ' -f1)
+printf "got checksum %s\n" "${hash}"
 target='3061da9b367ce0f6a21c94e7c74a016df49eed2ed274bae2a732d47a9655009b'
 if [ "${hash}" != "${target}" ]; then
 	printf "WARNING: maps27.jag's checksum is wrong\n" >/dev/stderr
