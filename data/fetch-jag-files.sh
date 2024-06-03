df1b9048a35e94b2a38aab1c7e5d3a6c309ce5aa
Stormy <stormy@mailfence.com>
2024-06-03 08:04:34 +0200

    fix script on linux (hopefully)

--- data/fetch-jag-files.sh
+++ data/fetch-jag-files.sh
@@ -14,14 +14,14 @@ else
 	exit 1
 fi
 
-if echo "a" | cksum -a sha256 -q 2>&1 >/dev/null; then
-	CKSUM="cksum -a sha256 -q"
-elif echo "a" | sha256 -q 2>&1 >/dev/null; then
+if echo "a" | sha256sum 2>/dev/null >/dev/null; then
+	CKSUM="sha256sum -b"
+elif echo "a" | gsha256sum 2>/dev/null >/dev/null; then
+	CKSUM="gsha256sum -b"
+elif echo "a" | sha256 -q 2>/dev/null >/dev/null; then
 	CKSUM="sha256 -q"
-elif echo "a" | sha256sum 2>&1 >/dev/null; then
-	CKSUM="sha256sum -b | cut -d' ' -f1"
-elif echo "a" | gsha256sum 2>&1 >/dev/null; then
-	CKSUM="gsha256sum -b | cut -d' ' -f1"
+elif echo "a" | cksum -a sha256 -q 2>/dev/null; then
+	CKSUM="cksum -a sha256 -q"
 else
 	printf "Please install a sha256sum utility and make sure" >/dev/stderr
 	printf "it's in your PATH.\n" >/dev/stderr
@@ -46,7 +46,7 @@ if ! [ -e config46.jag ]; then
 	get_url https://web.archive.org/web/20011212112437/http://66.28.11.53:80/client/config46.jag
 fi
 printf "verifying config46.jag\n"
-hash=$(${CKSUM} < config46.jag)
+hash=$(${CKSUM} < config46.jag | cut -d' ' -f1)
 target='304ff7147a21119c4e47f913f5d27d65de8222d69d2f3317dff569d8a4061e94'
 if [ "${hash}" != "${target}" ]; then
 	printf "WARNING: config46.jag's checksum is wrong\n" >/dev/stderr
@@ -60,7 +60,7 @@ if ! [ -e maps27.jag ]; then
 	get_url https://web.archive.org/web/20020108023203/http://209.120.137.226:80/client/maps27.jag
 fi
 printf "verifying maps27.jag\n"
-hash=$(${CKSUM} < maps27.jag)
+hash=$(${CKSUM} < maps27.jag | cut -d' ' -f1)
 target='3061da9b367ce0f6a21c94e7c74a016df49eed2ed274bae2a732d47a9655009b'
 if [ "${hash}" != "${target}" ]; then
 	printf "WARNING: maps27.jag's checksum is wrong\n" >/dev/stderr
