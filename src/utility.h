cda5045717bc9a8b429f4933e2b8ae05881e3692
Stormy <stormy@mailfence.com>
2024-06-04 00:05:20 +0200

    if !HAVE_REALLOCARR, define it in utility.h

--- src/utility.h
+++ src/utility.h
@@ -1,6 +1,11 @@
 #ifndef UTILITY_H
 #define UTILITY_H
 #include <stdint.h>
+#include "../config.h"
 
 char *mod37_namedec(int64_t, char *);
+
+#ifndef HAVE_REALLOCARR
+int reallocarr(void *, size_t, size_t);
+#endif
 #endif
