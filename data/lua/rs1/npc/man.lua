fe1797519f6959683a015efe94936191abe2095a
Stormy <stormy@mailfence.com>
2024-06-15 12:44:07 +0200

    man: use randomvar

--- data/lua/rs1/npc/man.lua
+++ data/lua/rs1/npc/man.lua
@@ -24,7 +24,8 @@ function talknpc_man1(player, npc)
 	say(player, "Hello")
 	say(player, "How's it going?")
 
-	if random(12) then
+	local rand = randomvar(21)
+	if rand == 0 then
 		npcsay(npc, "How can I help you?")
 		local opt = multi(player,
 			"Do you wish to trade?",
@@ -37,59 +38,59 @@ function talknpc_man1(player, npc)
 		elseif opt == 3 then
 			man1_enemies(player, npc)
 		end
-	elseif random(12) then
+	elseif rand == 1 then
 		npcsay(npc, "How can I help you?")
 		man1_trade(player, npc)
-	elseif random(12) then
+	elseif rand == 2 then
 		npcsay(npc, "How can I help you?")
 		man1_quest(player, npc)
-	elseif random(12) then
+	elseif rand == 3 then
 		npcsay(npc, "How can I help you?")
 		man1_enemies(player, npc)
-	elseif random(12) then
+	elseif rand == 4 then
 		npcsay(npc, "I'm a little worried")
 		npcsay(npc, "I've heard there's lots of people going about,")
 		npcsay(npc, "killing citizens at random")
-	elseif random(12) then
+	elseif rand == 5 then
 		npcsay(npc, "I'm a little worried about the increase in Goblins these days")
 		say(player, "Don't worry. I'll kill them")
-	elseif random(12) then
+	elseif rand == 6 then
 		npcsay(npc, "Not too bad")
-	elseif random(12) then
+	elseif rand == 7 then
 		mes(player, "@que@The man ignores you")
-	elseif random(12) then
+	elseif rand == 8 then
 		npcsay(npc, "None of your business")
-	elseif random(12) then
+	elseif rand == 9 then
 		npcsay(npc, "Get out of my way")
 		npcsay(npc, "I'm in a hurry")
-	elseif random(12) then
+	elseif rand == 10 then
 		npcsay(npc, "I'm fine")
 		npcsay(npc, "How are you?")
 		say(player, "Very well, thank you")
-	elseif random(12) then
+	elseif rand == 11 then
 		npcsay(npc, "Are you asking for a fight?")
 		npcattack(npc, player)
-	elseif random(12) then
+	elseif rand == 12 then
 		npcsay(npc, "Hello")
 		npcsay(npc, "Nice weather we've been having")
-	elseif random(12) then
+	elseif rand == 13 then
 		npcsay(npc, "That is classified information")
-	elseif random(12) then
+	elseif rand == 14 then
 		npcsay(npc, "Who are you?")
 		say(player, "I am a bold adventurer")
 		npcsay(npc, "A very noble profession")
-	elseif random(12) then
+	elseif rand == 15 then
 		npcsay(npc, "Do I know you?")
 		say(player, "No, I was just wondering if you had anything interesting to say")
-	elseif random(12) then
+	elseif rand == 16 then
 		npcsay(npc, "Have this flier")
 		give(player, "flier", 1)
-	elseif random(12) then
+	elseif rand == 17 then
 		npcsay(npc, "I think we need a new king")
 		npcsay(npc, "The one we've got isn't very good")
-	elseif random(12) then
+	elseif rand == 18 then
 		npcsay(npc, "No, I don't have any spare change")
-	elseif random(12) then
+	elseif rand == 19 then
 		npcsay(npc, "No, I don't want to buy anything")
 	else
 		npcsay(npc, "Hello")
