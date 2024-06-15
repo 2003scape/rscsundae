0fbf2fa89972801f5c179c1d2355e34287dc3d8a
Stormy <stormy@mailfence.com>
2024-06-15 09:37:51 +0200

    more delays idk

--- data/lua/rs1/misc/doors.lua
+++ data/lua/rs1/misc/doors.lua
@@ -1,6 +1,7 @@
 function opbound1_normdoor(player, x, y, dir)
 	mes(player, "@que@The door swings open")
 	changebound(x, y, dir, "normdoorframe")
+	delay(4)
 end
 
 function opbound2_normdoorframe(player, x, y, dir)
@@ -12,6 +13,7 @@ end
 function opbound1_highdoor(player, x, y, dir)
 	mes(player, "@que@The door swings open")
 	changebound(x, y, dir, "highdoorframe")
+	delay(4)
 end
 
 function opbound2_highdoorframe(player, x, y, dir)
