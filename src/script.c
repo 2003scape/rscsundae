65186e7d7cfb34b70193a3ad4b2356568fcca268
Stormy <stormy@mailfence.com>
2024-06-20 08:57:10 +0200

    script: implement statatleast

--- src/script.c
+++ src/script.c
@@ -32,6 +32,7 @@ static int script_substat(lua_State *);
 static int script_npchealstat(lua_State *);
 static int script_npcaddstat(lua_State *);
 static int script_npcsubstat(lua_State *);
+static int script_statatleast(lua_State *);
 static int script_statup(lua_State *);
 static int script_statdown(lua_State *);
 static int script_statrandom(lua_State *);
@@ -547,6 +548,32 @@ script_addstat(lua_State *L)
 	stat_add(&p->mob, stat, constant, percent);
 	player_send_stat(p, stat);
 	return 0;
+}
+
+static int
+script_statatleast(lua_State *L)
+{
+	lua_Integer player_id, stat, low;
+	struct player *p;
+	int b;
+
+	player_id = luaL_checkinteger(L, 1);
+	stat = luaL_checkinteger(L, 2);
+	low = luaL_checkinteger(L, 3);
+	p = id_to_player(player_id);
+	if (p == NULL) {
+		printf("script warning: player %lld is undefined\n", player_id);
+		script_cancel(L, player_id);
+		return 0;
+	}
+	if (stat < 0 || stat >= MAX_SKILL_ID) {
+		printf("script warning: invalid stat id %lld\n", stat);
+		script_cancel(L, player_id);
+		return 0;
+	}
+	b = p->mob.cur_stats[stat] >= low;
+	lua_pushboolean(L, b);
+	return 1;
 }
 
 static int
@@ -1627,6 +1654,9 @@ script_init(struct server *s)
 
 	lua_pushcfunction(L, script_healstat);
 	lua_setglobal(L, "healstat");
+
+	lua_pushcfunction(L, script_statatleast);
+	lua_setglobal(L, "statatleast");
 
 	lua_pushcfunction(L, script_statup);
 	lua_setglobal(L, "statup");
