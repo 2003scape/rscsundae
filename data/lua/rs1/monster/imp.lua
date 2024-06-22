3f60a8651688c0bd4ff7b9b8600f694f3aaadada
Stormy <stormy@mailfence.com>
2024-06-22 22:11:49 +0200

    lua: add drop table for imps (from frogdoubler, slightly modified)

---
+++ data/lua/rs1/monster/imp.lua
@@ -0,0 +1,59 @@
+-- https://classic.runescape.wiki/w/Imp#Drops
+
+local drops = {
+	-- beads
+	{ "black bead", 1, 5 },
+	{ "red bead", 1, 5 },
+	{ "white bead", 1, 5 },
+	{ "yellow bead", 1, 5 },
+
+	-- ammunition
+	{ "crossbow bolts", 1, 8 },
+
+	-- foodstuffs
+	{ "grain", 1, 10 },
+	{ "egg", 1, 5 },
+	{ "raw chicken", 1, 5 },
+	{ "burntbread", 1, 4 },
+	{ "burntmeat", 1, 4 },
+	{ "bread dough", 1, 2 },
+	{ "cabbage", 1, 2 },
+	{ "flour", 1, 2 },
+	{ "bread", 1, 1 },
+	{ "cookedmeat", 1, 1 },
+
+	-- other
+	{ "ball of wool", 1, 8 },
+	{ "hammer", 1, 8 },
+	{ "bluewizhat", 1, 8 },
+	{ "ashes", 1, 6 },
+	{ "tinderbox", 1, 5 },
+	{ "bucket", 1, 4 },
+	{ "cadavaberries", 1, 4 },
+	{ "clay", 1, 4 },
+	{ "shears", 1, 4 },
+	{ "chefshat", 1, 2 },
+	{ "flier", 1, 2 },
+	{ "jug", 1, 2 },
+	{ "pot", 1, 2 },
+	{ "waterbucket", 1, 2 },
+	{ "waterjug", 1, 2 },
+	{ "acnepotion", 1, 1 }
+}
+
+function killnpc_imp(player, npc, x, y)
+	local ran = randomvar(128)
+
+	local weight = 0
+
+	addobject(player, "ashes", 1, x, y)
+
+	for _, drop in ipairs(drops) do
+		weight = weight + drop[3]
+
+		if ran < weight then
+			addobject(player, drop[1], drop[2], x, y)
+			break
+		end
+	end
+end
--- data/lua/script.lua
+++ data/lua/script.lua
@@ -555,6 +555,7 @@ function multi(player, ...)
 end
 
 dofile("./lua/rs1/monster/chicken.lua")
+dofile("./lua/rs1/monster/imp.lua")
 dofile("./lua/rs1/npc/asgarnia/barmaid.lua")
 dofile("./lua/rs1/npc/asgarnia/betty.lua")
 dofile("./lua/rs1/npc/asgarnia/brian.lua")
