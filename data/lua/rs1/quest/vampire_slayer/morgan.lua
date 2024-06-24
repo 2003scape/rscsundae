17795f41b70c96074014908b6d4df8306f5d5c54
Mister Hat <misterhat144@gmail.com>
2024-06-24 18:28:59 -0500

    fix formatting for morgan

--- data/lua/rs1/quest/vampire_slayer/morgan.lua
+++ data/lua/rs1/quest/vampire_slayer/morgan.lua
@@ -41,24 +41,24 @@ function talknpc_morgan(player, npc)
 			npcsay(npc, "I don't blame you")
 		elseif resp == 2 then
 			say(player, "Ok I'm up for an adventure")
-            morgan_seek_help(player, npc)
-        elseif resp == 3 then
-            say(player, "I tried fighting him. He wouldn't die")
-            npcsay(npc, "Maybe you are not going about it right")
-            morgan_seek_help(player, npc)
+			morgan_seek_help(player, npc)
+		elseif resp == 3 then
+			say(player, "I tried fighting him. He wouldn't die")
+			npcsay(npc, "Maybe you are not going about it right")
+			morgan_seek_help(player, npc)
 		end
-    else
-        npcsay(npc, "How are you doing with your quest?")
+	else
+		npcsay(npc, "How are you doing with your quest?")
 
-        if stage == 1 then
-            say(player, "I'm working on it still")
-            npcsay(npc, "Please hurry")
-            npcsay(npc, "Every day we live in fear of lives")
-            npcsay(npc, "That we will be the vampires next victim")
-        elseif stage == 2 then
-            say(player, "I have slain the foul creature")
-            npcsay(npc, "Thank you, thank you")
-            npcsay(npc, "You will always be a hero in our village")
-        end
+		if stage == 1 then
+			say(player, "I'm working on it still")
+			npcsay(npc, "Please hurry")
+			npcsay(npc, "Every day we live in fear of lives")
+			npcsay(npc, "That we will be the vampires next victim")
+		elseif stage == 2 then
+			say(player, "I have slain the foul creature")
+			npcsay(npc, "Thank you, thank you")
+			npcsay(npc, "You will always be a hero in our village")
+		end
 	end
 end
