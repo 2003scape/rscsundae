8d7fd85880ab16dd7196e688c7c672f130ea6cd7
Stormy <stormy@mailfence.com>
2024-06-24 15:46:36 +0200

    clutter: implement coffin opening/closing/searching

--- data/lua/rs1/misc/clutter.lua
+++ data/lua/rs1/misc/clutter.lua
@@ -29,3 +29,20 @@ function oploc1_cupboardshut(player, x, y)
 	mes(player, "@que@You open the cupboard")
 	changeloc(x, y, "cupboardopen")
 end
+
+-- Logg/Tylerbeg/08-02-2018 03.50.02 barbarian village and edgeville
+function oploc1_shutcoffin(player, x, y)
+	mes(player, "You open the coffin")
+	changeloc(x, y, "opencoffin")
+end
+
+-- Logg/Tylerbeg/08-02-2018 03.50.02 barbarian village and edgeville
+function oploc1_opencoffin(player, x, y)
+	mes(player, "You search the coffin and find some human remains")
+end
+
+-- Logg/Tylerbeg/06-14-2018 18.58.03 fighting chaos druids for 3 hours.pcap
+function oploc2_opencoffin(player, x, y)
+	mes(player, "You close the coffin")
+	changeloc(x, y, "shutcoffin")
+end
