575bf38c461afb5f8e00f42fc85d6979bd2ca657
Stormy <stormy@mailfence.com>
2024-06-25 09:05:54 +0200

    cupboard: more formatting

--- data/lua/rs1/quest/vampire_slayer/cupboard.lua
+++ data/lua/rs1/quest/vampire_slayer/cupboard.lua
@@ -1,3 +1,5 @@
+-- rsc-preservation.xyz/Quests/Vampire Slayer
+
 function oploc1_garliccupboardshut(player, x, y)
 	mes(player, "You open the cupboard")
 	changeloc(x, y, "garliccupboardopen")
@@ -7,10 +9,10 @@ function oploc1_garliccupboardopen(player, x, y)
 	mes(player, "You search the cupboard")
 
 	if held(player, "garlic", 1) then
-	    mes(player, "The cupboard is empty")
+		mes(player, "The cupboard is empty")
 	else
-	    mes(player, "You find a clove of garlic that you take")
-	    give(player, "garlic", 1)
+		mes(player, "You find a clove of garlic that you take")
+		give(player, "garlic", 1)
 	end
 end
 
