dc338870cc636418d1266cdd6a106da6a656c798
Stormy <stormy@mailfence.com>
2024-06-25 09:01:22 +0200

    cupboard: format

--- data/lua/rs1/quest/vampire_slayer/cupboard.lua
+++ data/lua/rs1/quest/vampire_slayer/cupboard.lua
@@ -1,20 +1,20 @@
 function oploc1_garliccupboardshut(player, x, y)
-    mes(player, "You open the cupboard")
-    changeloc(x, y, "garliccupboardopen")
+	mes(player, "You open the cupboard")
+	changeloc(x, y, "garliccupboardopen")
 end
 
 function oploc1_garliccupboardopen(player, x, y)
-    mes(player, "You search the cupboard")
+	mes(player, "You search the cupboard")
 
-    if held(player, "garlic", 1) then
-        mes(player, "The cupboard is empty")
-    else
-        mes(player, "You find a clove of garlic that you take")
-        give(player, "garlic", 1)
-    end
+	if held(player, "garlic", 1) then
+	    mes(player, "The cupboard is empty")
+	else
+	    mes(player, "You find a clove of garlic that you take")
+	    give(player, "garlic", 1)
+	end
 end
 
 function oploc2_garliccupboardopen(player, x, y)
-    changeloc(x, y, "garliccupboardshut")
-    mes(player, "You close the cupboard")
+	changeloc(x, y, "garliccupboardshut")
+	mes(player, "You close the cupboard")
 end
