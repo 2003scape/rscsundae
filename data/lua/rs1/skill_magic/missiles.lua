2dd1bb328ea0ab7e6093b9610856a68a0d1af3b4
Stormy <stormy@mailfence.com>
2024-06-29 10:15:30 +0200

    missiles: use anonymous functions

--- data/lua/rs1/skill_magic/missiles.lua
+++ data/lua/rs1/skill_magic/missiles.lua
@@ -140,130 +140,117 @@ function skillplayer_wind_strike(player, target)
 	finalize_wind_strike(player)
 end
 
-function skillnpc_wind_strike(player, target)
+register_skillnpc("_", "wind strike", function(player, target)
 	shootnpc(player, target, "missile01")
 	finalize_wind_strike(player)
-end
+end)
 
 function skillplayer_water_strike(player, target)
 	shootplayer(player, target, "missile02")
 	finalize_water_strike(player)
 end
 
-function skillnpc_water_strike(player, target)
+register_skillnpc("_", "water strike", function(player, target)
 	shootnpc(player, target, "missile02")
 	finalize_water_strike(player)
-end
+end)
 
 function skillplayer_earth_strike(player, target)
 	shootplayer(player, target, "missile03")
 	finalize_earth_strike(player)
 end
 
-function skillnpc_earth_strike(player, target)
+register_skillnpc("_", "earth strike", function(player, target)
 	shootnpc(player, target, "missile03")
 	finalize_earth_strike(player)
-end
+end)
 
 function skillplayer_fire_strike(player, target)
 	shootplayer(player, target, "missile04")
 	finalize_fire_strike(player)
 end
 
-function skillnpc_fire_strike(player, target)
+register_skillnpc("_", "fire strike", function(player, target)
 	shootnpc(player, target, "missile04")
 	finalize_fire_strike(player)
-end
+end)
 
 function skillplayer_wind_bolt(player, target)
 	shootplayer(player, target, "missile05")
 	finalize_wind_bolt(player)
 end
 
-function skillnpc_wind_bolt(player, target)
+register_skillnpc("_", "wind bolt", function(player, target)
 	shootnpc(player, target, "missile05")
 	finalize_wind_bolt(player)
-end
+end)
 
 function skillplayer_water_bolt(player, target)
 	shootplayer(player, target, "missile06")
 	finalize_water_bolt(player)
 end
 
-function skillnpc_water_bolt(player, target)
+register_skillnpc("_", "water bolt", function(player, target)
 	shootnpc(player, target, "missile06")
 	finalize_water_bolt(player)
-end
+end)
 
 function skillplayer_earth_bolt(player, target)
 	shootplayer(player, target, "missile07")
 	finalize_earth_bolt(player)
 end
 
-function skillnpc_earth_bolt(player, target)
+register_skillnpc("_", "earth bolt", function(player, target)
 	shootnpc(player, target, "missile07")
 	finalize_earth_bolt(player)
-end
+end)
 
 function skillplayer_fire_bolt(player, target)
 	shootplayer(player, target, "missile08")
 	finalize_fire_bolt(player)
 end
 
-function skillnpc_fire_bolt(player, target)
+register_skillnpc("_", "fire bolt", function(player, target)
 	shootnpc(player, target, "missile08")
 	finalize_fire_bolt(player)
-end
+end)
 
 function skillplayer_wind_blast(player, target)
 	shootplayer(player, target, "missile09")
 	finalize_wind_blast(player)
 end
 
-function skillnpc_wind_blast(player, target)
+register_skillnpc("_", "wind blast", function(player, target)
 	shootnpc(player, target, "missile09")
 	finalize_wind_blast(player)
-end
+end)
 
 function skillplayer_water_blast(player, target)
 	shootplayer(player, target, "missile10")
 	finalize_water_blast(player)
 end
 
-function skillnpc_water_blast(player, target)
+register_skillnpc("_", "water blast", function(player, target)
 	shootnpc(player, target, "missile10")
 	finalize_water_blast(player)
-end
+end)
 
 function skillplayer_earth_blast(player, target)
 	shootplayer(player, target, "missile11")
 	finalize_earth_blast(player)
 end
 
-function skillnpc_earth_blast(player, target)
+register_skillnpc("_", "earth blast", function(player, target)
 	shootnpc(player, target, "missile11")
 	finalize_earth_blast(player)
-end
+end)
 
 function skillplayer_fire_blast(player, target)
 	shootplayer(player, target, "missile12")
 	finalize_fire_blast(player)
 end
 
-function skillnpc_fire_blast(player, target)
+register_skillnpc("_", "fire blast", function(player, target)
 	shootnpc(player, target, "missile12")
 	finalize_fire_blast(player)
-end
-
-register_skillnpc("_", "wind strike", skillnpc_wind_strike)
-register_skillnpc("_", "water strike", skillnpc_water_strike)
-register_skillnpc("_", "earth strike", skillnpc_earth_strike)
-register_skillnpc("_", "fire strike", skillnpc_fire_strike)
-register_skillnpc("_", "wind bolt", skillnpc_wind_bolt)
-register_skillnpc("_", "water bolt", skillnpc_water_bolt)
-register_skillnpc("_", "earth bolt", skillnpc_earth_bolt)
-register_skillnpc("_", "fire bolt", skillnpc_fire_bolt)
-register_skillnpc("_", "wind blast", skillnpc_wind_blast)
-register_skillnpc("_", "water blast", skillnpc_water_blast)
-register_skillnpc("_", "earth blast", skillnpc_earth_blast)
-register_skillnpc("_", "fire blast", skillnpc_fire_blast)
+end)
