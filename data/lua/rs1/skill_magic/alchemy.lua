dcafcad7f25faa5802810411117c8f708ff6b2e6
Stormy <stormy@mailfence.com>
2024-06-29 10:20:43 +0200

    alchemy: use anonymous functions

--- data/lua/rs1/skill_magic/alchemy.lua
+++ data/lua/rs1/skill_magic/alchemy.lua
@@ -2,7 +2,7 @@
 
 -- TODO: onobj message, "You must be holding the object to use this spell"
 
-function low_alchemy(player, item)
+register_spellinv("_", "low level alchemy", function(player, item)
 	mes(player, "@que@Alchemy spell successful")
 	sellinv(player, item, 40)
 	advancestat(player, STAT_MAGIC, 248, 0)
@@ -10,9 +10,9 @@ function low_alchemy(player, item)
 		remove(player, "fire-rune", 3)
 	end
 	remove(player, "nature-rune", 1)
-end
+end)
 
-function high_alchemy(player, item)
+register_spellinv("_", "high level alchemy", function(player, item)
 	mes(player, "@que@Alchemy spell successful")
 	sellinv(player, item, 60)
 	advancestat(player, STAT_MAGIC, 520, 0)
@@ -20,7 +20,4 @@ function high_alchemy(player, item)
 		remove(player, "fire-rune", 5)
 	end
 	remove(player, "nature-rune", 1)
-end
-
-register_spellinv("_", "low level alchemy", low_alchemy)
-register_spellinv("_", "high level alchemy", high_alchemy)
+end)
