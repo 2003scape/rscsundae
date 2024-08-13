1a9df36f0d13c9039e4c638a4522013687edd96f
Stormy <stormy@mailfence.com>
2024-08-13 11:39:50 +0200

    teleport: centralize quest anti-cheat

--- data/lua/rs1/skill_magic/teleport.lua
+++ data/lua/rs1/skill_magic/teleport.lua
@@ -1,6 +1,4 @@
 -- teleport spells have no message to confirm they worked
--- for karamja rum effect, see:
--- RSC 2001/7/iteminfo- karamja rum- teleport from port sarim to falador.pcap
 
 local function remove_teleport_runes(player, primary_rune_name)
 	if not worn(player, "staff of air") then
@@ -10,6 +8,16 @@ local function remove_teleport_runes(player, primary_rune_name)
 		remove(player, primary_rune_name .. "-rune", 1)
 	end
 	remove(player, "law-rune", 1)
+
+	-- for karamja rum effect, see:
+	-- RSC 2001/7/iteminfo- karamja rum- teleport from port sarim to falador.pcap
+	remove(player, "karamja rum", 30)
+	setvar(player, "ernest_lever_a", 0)
+	setvar(player, "ernest_lever_b", 0)
+	setvar(player, "ernest_lever_c", 0)
+	setvar(player, "ernest_lever_d", 0)
+	setvar(player, "ernest_lever_e", 0)
+	setvar(player, "ernest_lever_f", 0)
 end
 
 local function teleport_allowed(player)
@@ -27,7 +35,6 @@ function spellself_varrock_teleport(player)
 	-- RSC 2001/replays master archive/Skilling/Magic/spell- law- teleport to varrock
 	if teleport_allowed(player) then
 		teleport(player, 120, 504)
-		remove(player, "karamja rum", 30)
 		advancestat(player, STAT_MAGIC, 280, 0)
 		remove_teleport_runes(player, "fire")
 		showeffect(0, 120, 503)
@@ -38,7 +45,6 @@ function spellself_lumbridge_teleport(player)
 	-- RSC 2001/replays master archive/Skilling/Magic/spell- law- teleport to lumbridge
 	if teleport_allowed(player) then
 		teleport(player, 120, 648)
-		remove(player, "karamja rum", 30)
 		advancestat(player, STAT_MAGIC, 328, 0)
 		remove_teleport_runes(player, "earth")
 		showeffect(0, 120, 647)
@@ -49,7 +55,6 @@ function spellself_falador_teleport(player)
 	-- RSC 2001/replays master archive/Skilling/Magic/spell- law- teleport to falador
 	if teleport_allowed(player) then
 		teleport(player, 312, 552)
-		remove(player, "karamja rum", 30)
 		advancestat(player, STAT_MAGIC, 376, 0)
 		remove_teleport_runes(player, "water")
 		showeffect(0, 312, 551)
