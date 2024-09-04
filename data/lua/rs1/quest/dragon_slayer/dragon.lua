1d329f1b5a5ed3009008fcc98fba1ba7b81b7468
Stormy <stormy@mailfence.com>
2024-09-04 21:04:45 +0200

    dragon: implement dragon breath mechanics

--- data/lua/rs1/quest/dragon_slayer/dragon.lua
+++ data/lua/rs1/quest/dragon_slayer/dragon.lua
@@ -1,4 +1,22 @@
 -- https://classic.runescape.wiki/w/Dragon_(Dragon_slayer)
+-- 1e_Luis/Quests/Dragon Slayer.pcap
+-- Revisionism/06-16-2018 01.57.46-dragon-slayer.pcap
+-- ShaunDreclin/06-08-2018 23.16.26 dragon slayer.pcap
+
+function attacknpc_questdragon(player, npc)
+	mes(player, "@que@The dragon breathes fire at you")
+	if worn(player, "anti dragon breath shield") then
+		mes(player, "@que@Your shield prevents some of the damage from the flames")
+		substat(player, STAT_HITS, randomvar(13), 0)
+	else
+		mes(player, "@que@You are fried")
+		substat(player, STAT_HITS, randomvar(65), 0)
+	end
+	-- revisionism goes from 46/46 prayer to 10/46
+	-- ShaunDreclin goes from 35/42 prayer to 7/42
+	-- assume 80% prayer reduction
+	substat(player, STAT_PRAYER, 1, 80)
+end
 
 function killnpc_questdragon(player, npc, x, y)
 	if getvar(player, "dragon_stage") == 3 then
